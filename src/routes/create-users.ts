afbba4bcd49af38a58f6aeac151145b258acfdd4
Walison da Silva Lima <wallisonlpq@gmail.com>
2025-08-19 13:09:21 -0300

    ... create-user

--- src/routes/create-users.ts
+++ src/routes/create-users.ts
@@ -1,37 +1,24 @@
 import {type FastifyPluginAsyncZod } from "fastify-type-provider-zod";
+import { z } from "zod";
 
 const createUsersApp: FastifyPluginAsyncZod = async (server) => {
-  server.post('/users', {
+  server.post("/users", {
     schema: {
-      body: {
-        type: 'object',
-        properties: {
-          name: { type: 'string' },
-          email: { type: 'string', format: 'email' }
-        },
-        required: ['name', 'email']
-      },
-        reply: {
-            200: {
-            type: 'object',
-            properties: {
-                message: { type: 'string' },
-                user: {
-                type: 'object',
-                properties: {
-                    name: { type: 'string' },
-                    email: { type: 'string', format: 'email' }
-                }
-                }
-            }
-            }
-        }
-    }       
-  }, async (request, reply) => {
-    const { name, email } = request.body as { name: string; email: string };
-    
-    // Here you would typically save the user to a database
-    // For this example, we will just return the user data
-    return { message: 'User created successfully', user: { name, email } };
-  });
+      body: z.object({
+        name: z.string(),
+        email: z.email(),
+        password: z.string().min(6),
+      }),
+      response: {
+        200: z.object({
+          cursirId: z.ulid().describe("Cursor criado com sucesso")
+        })
+      }
+    }
+  }, (request, reply)=> {
+    const {name, email, password} = request.body
+
+  })
 }
+
+export {createUsersApp}
